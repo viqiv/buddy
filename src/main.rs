f75b9a0bc17e78562a1332ab494f514176dc5779
nturume <nturumezz@gmail.com>
2025-11-19 20:39:47 +0300

    refact

--- src/main.rs
+++ src/main.rs
@@ -33,53 +33,20 @@ struct Page {
     ref_cnt: usize,
     ord: usize,
     next: Option<NonNull<Page>>,
-    prev: Option<NonNull<Page>>,
 }
 
 impl Page {
-    fn new(idx: usize, ord: usize) -> Page {
+    fn new(idx: usize, ord: usize, rc: usize) -> Page {
         Page {
             idx,
-            ref_cnt: 0,
+            ref_cnt: rc,
             ord,
             next: None,
-            prev: None,
-        }
-    }
-
-    fn append(&mut self, p: *mut Page) {
-        assert!(self as *mut Page != p);
-        let r = unsafe { p.as_mut() }.unwrap();
-        r.prev = NonNull::new(self as *mut Page);
-        self.next = NonNull::new(p);
-    }
-
-    fn print_list(&self) {
-        let mut ptr = Some(self);
-        while let Some(p) = ptr {
-            println!("{:?}", p);
-            ptr = match p.next {
-                Some(n) => Some(unsafe { n.as_ref() }),
-                _ => None,
-            };
-        }
-    }
-
-    fn remove(&mut self) {
-        if let Some(mut nxt) = self.next {
-            let nxt = unsafe { nxt.as_mut() };
-            nxt.prev = self.prev
-        }
-
-        if let Some(mut prev) = self.prev {
-            let prev = unsafe { prev.as_mut() };
-            prev.next = self.next
         }
     }
 
     fn rm_links(&mut self) {
         self.next = None;
-        self.prev = None;
     }
 
     fn split(&mut self, order: usize, idx: usize, a: &mut Allocator) {
@@ -87,11 +54,7 @@ impl Page {
         let len = (4096 << (Allocator::ORDER - order)) / 2;
         let p_idx_1 = self.idx;
         let p_idx_2 = self.idx + (len / 4096);
-        if idx == 0 {
-            a.free_lists[order] = self.next;
-        }
-        self.remove();
-        self.rm_links();
+        a.free_lists[order].remove(self);
         let ptr1 = unsafe { a.ptr.add(p_idx_1) };
         let ptr2 = unsafe { a.ptr.add(p_idx_2) };
         let ptr1_r = unsafe { ptr1.as_mut() }.unwrap();
@@ -100,30 +63,12 @@ impl Page {
         assert!(ptr2_r.idx == p_idx_2);
         ptr1_r.ord = order + 1;
         ptr2_r.ord = order + 1;
-        unsafe { ptr1.as_mut() }.unwrap().append(ptr2);
-        match a.free_lists[order + 1] {
-            Some(f) => {
-                unsafe { ptr2.as_mut() }.unwrap().append(f.as_ptr());
-                a.free_lists[order + 1] = NonNull::new(ptr1);
-            }
-            None => {
-                a.free_lists[order + 1] = NonNull::new(ptr1);
-            }
-        }
+        a.free_lists[order + 1].add(ptr2);
+        a.free_lists[order + 1].add(ptr1);
     }
 
     fn is_idle(&self) -> bool {
         self.ref_cnt == 0
-    }
-
-    fn add_to_fl(&mut self, alloc: &mut Allocator) {
-        match alloc.free_lists[self.ord] {
-            Some(s) => {
-                self.append(s.as_ptr());
-            }
-            _ => {}
-        }
-        alloc.free_lists[self.ord] = NonNull::new(self as *mut Page);
     }
 
     fn join(&mut self, alloc: &mut Allocator) {
@@ -131,22 +76,17 @@ impl Page {
 
         loop {
             if page.ord == 0 {
-                return page.add_to_fl(alloc);
+                return alloc.free_lists[page.ord].add(page);
             }
             let addr = page.idx * 4096;
             let b_idx = Allocator::get_buddy(addr, page.ord) / 4096;
             let buddy_ptr = unsafe { alloc.ptr.add(b_idx) };
             let buddy = unsafe { buddy_ptr.as_mut() }.unwrap();
             if buddy.ord != page.ord || !buddy.is_idle() {
-                return page.add_to_fl(alloc);
+                return alloc.free_lists[page.ord].add(page);
             }
 
-            buddy.remove();
-
-            if alloc.free_lists[page.ord].is_some_and(|x| x.as_ptr() == buddy_ptr) {
-                alloc.free_lists[page.ord] = buddy.next;
-            }
-            buddy.rm_links();
+            alloc.free_lists[page.ord].remove(buddy);
             let merg = if page.idx > buddy.idx { buddy } else { page };
             merg.ord -= 1;
             page = merg;
@@ -161,13 +101,79 @@ impl Default for Page {
             ref_cnt: 0,
             ord: 0,
             next: None,
-            prev: None,
+        }
+    }
+}
+
+#[derive(Default)]
+struct FL {
+    head: Option<NonNull<Page>>,
+}
+
+impl FL {
+    fn remove(&mut self, node: &mut Page) {
+        assert!(self.head.is_some());
+        let mut h = unsafe { self.head.unwrap().as_mut() };
+        if h.idx == node.idx {
+            self.head = h.next;
+            h.next = None;
+            return;
+        }
+
+        while let Some(mut nxt) = h.next {
+            let nxt = unsafe { nxt.as_mut() };
+            if nxt.idx == node.idx {
+                h.next = node.next;
+                node.next = None;
+                return;
+            }
+            h = nxt;
+        }
+
+        unreachable!()
+    }
+
+    fn add(&mut self, node: *mut Page) {
+        let node = unsafe { node.as_mut().unwrap() };
+        node.next = self.head;
+        self.head = NonNull::new(node as *mut Page);
+    }
+
+    fn rm_first(&mut self) -> Option<*mut Page> {
+        match self.head {
+            Some(mut h) => {
+                let h = unsafe { h.as_mut() };
+                self.head = h.next;
+                Some(h)
+            }
+            _ => None,
+        }
+    }
+
+    fn get_head(&self) -> Option<*mut Page> {
+        match self.head {
+            Some(mut h) => Some(unsafe { h.as_mut() }),
+            _ => None,
+        }
+    }
+
+    fn print_list(&self) {
+        if self.head.is_none() {
+            return;
+        }
+        let mut ptr = Some(unsafe { self.head.unwrap().as_ref() });
+        while let Some(p) = ptr {
+            println!("{:?}", p);
+            ptr = match p.next {
+                Some(n) => Some(unsafe { n.as_ref() }),
+                _ => None,
+            };
         }
     }
 }
 
 struct Allocator {
-    free_lists: [Option<NonNull<Page>>; 9],
+    free_lists: [FL; 9],
     ptr: *mut Page,
     size: usize,
     meta_size: usize,
@@ -177,12 +183,12 @@ impl Allocator {
     const ORDER: usize = 8;
 
     fn new(ptr: *mut u8, len: usize) -> Allocator {
-        assert!(len.count_ones() == 1);
+        assert!(len % 4096 == 0);
         let npages = len / 4096;
         let page_ptr = ptr as *mut Page;
         for i in 0..npages {
             unsafe {
-                *page_ptr.add(i) = Page::new(i, 0);
+                *page_ptr.add(i) = Page::new(i, 8, 1);
             }
         }
         let mut alloc = Allocator {
@@ -196,25 +202,12 @@ impl Allocator {
     }
 
     fn init_free_list(&mut self) {
-        let big_ord = 4096 << Self::ORDER;
-        let meta_pages = align_f(self.meta_size, big_ord) / big_ord;
-        let total_pages = self.size / big_ord;
-        let mut prev: Option<NonNull<Page>> = None;
-        for i in meta_pages..total_pages {
-            let p_idx = (big_ord / 4096) * i;
-            match self.free_lists[0] {
-                Some(_) => {
-                    let prev_ref = unsafe { prev.unwrap().as_mut() };
-                    let nxt = unsafe { self.ptr.add(p_idx) };
-                    prev_ref.append(nxt);
-                    prev = NonNull::new(nxt);
-                }
-                None => {
-                    assert!(i == meta_pages);
-                    let p = unsafe { self.ptr.add(p_idx) };
-                    self.free_lists[0] = NonNull::new(p);
-                    prev = self.free_lists[0];
-                }
+        let mut i = align_f(self.meta_size, 4096);
+        loop {
+            self.free((i + self.ptr as usize) as *mut u8);
+            i += 4096;
+            if i == self.size {
+                break;
             }
         }
     }
@@ -223,26 +216,22 @@ impl Allocator {
         8 - (align_f(n, 4096).next_power_of_two().ilog2() - 12) as usize
     }
 
-    fn remove_first(&mut self, fidx: usize) -> Option<*mut Page> {
-        match self.free_lists[fidx] {
-            Some(mut f) => {
-                let fr = unsafe { f.as_mut() };
-                self.free_lists[fidx] = fr.next;
-                fr.remove();
-                Some(f.as_ptr())
-            }
-            _ => None,
-        }
-    }
-
     fn split_to(&mut self, page: &mut Page, mut cur_ord: usize, t_ord: usize) {
         assert!(t_ord > cur_ord);
         let mut page_tmp = page;
+        let oc = cur_ord;
         while t_ord > cur_ord {
             page_tmp.split(cur_ord, 0, self);
-            page_tmp = unsafe { self.free_lists[cur_ord + 1].unwrap().as_mut() };
+            page_tmp = unsafe {
+                self.free_lists[cur_ord + 1]
+                    .get_head()
+                    .unwrap()
+                    .as_mut()
+                    .unwrap()
+            };
             cur_ord += 1;
         }
+        assert!(oc != cur_ord);
     }
 
     fn _alloc(&mut self, n: usize) -> Option<*mut u8> {
@@ -252,7 +241,7 @@ impl Allocator {
 
         let ord = Self::get_ord(n) as usize;
 
-        if let Some(p) = self.remove_first(ord) {
+        if let Some(p) = self.free_lists[ord].rm_first() {
             let p = unsafe { p.as_mut() }.unwrap();
             p.ref_cnt += 1;
             p.rm_links();
@@ -265,7 +254,7 @@ impl Allocator {
 
         let mut i = ord - 1;
         loop {
-            if let Some(p) = self.remove_first(i) {
+            if let Some(p) = self.free_lists[i].get_head() {
                 self.split_to(unsafe { p.as_mut().unwrap() }, i, ord);
                 break;
             }
@@ -275,7 +264,7 @@ impl Allocator {
             i -= 1;
         }
 
-        if let Some(p) = self.remove_first(ord) {
+        if let Some(p) = self.free_lists[ord].rm_first() {
             let p = unsafe { p.as_mut() }.unwrap();
             p.ref_cnt += 1;
             p.rm_links();
@@ -301,7 +290,7 @@ impl Allocator {
         assert!(addr < self.size);
         let idx = addr / 4096;
         let page = unsafe { self.ptr.add(idx).as_mut() }.unwrap();
-        assert!(page.next.is_none() && page.prev.is_none());
+        assert!(page.next.is_none());
         page.ref_cnt -= 1;
         if page.ref_cnt > 0 {
             return;
@@ -316,7 +305,7 @@ struct U32buff {
 }
 fn rand_u32() -> usize {
     let mut buf = U32buff { buf: [0, 0, 0, 0] };
-    let mut r = File::open("/dev/random").unwrap();
+    let mut r = File::open("/dev/urandom").unwrap();
     r.read_exact(&mut buf.buf).unwrap();
     unsafe { *(buf.buf.as_ptr() as *const u32) as usize }
 }
@@ -325,13 +314,14 @@ fn main() {
     let mut a = Allocator::new(unsafe { BUF.as_mut_ptr() }, unsafe { BUF.len() });
     let mut h = vec![];
 
-    for _ in 0..10 {
+    for _ in 0..100 {
         loop {
             let size = 4096 << (rand_u32() % Allocator::ORDER);
             if let Some(a) = a.alloc(rand_u32() % size) {
                 unsafe { *a = 69 };
                 h.push(a);
             } else {
+                // println!("ptr = {:?}", a);
                 break;
             };
         }
@@ -344,9 +334,6 @@ fn main() {
 
     for i in 0..9 {
         println!("+++++++++FREELIST [{}]+++++++++++", i);
-        match a.free_lists[i] {
-            Some(f) => unsafe { f.as_ref() }.print_list(),
-            None => {}
-        }
+        a.free_lists[i].print_list();
     }
 }
